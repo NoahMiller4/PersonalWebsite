3783b6d15f68868a3a979cb9e1a432b009f4c260
Noah Miller <Noseby@live.ca>
2023-08-31 13:10:59 -0500

    updated style

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,3 +7,18 @@
     Assignment 4
 */
 
+/* -----------------------------------*/
+/*  Smooth Scrolling                  */
+/* -----------------------------------*/
+
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
